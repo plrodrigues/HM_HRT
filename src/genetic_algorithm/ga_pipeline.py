03184a50b338d52d11210489b90414617d09a6ae
PLRodrigues <35863590+plrodrigues@users.noreply.github.com>
2023-02-23 23:57:21 +0000

    add instance id to the main results structure

--- src/genetic_algorithm/ga_pipeline.py
+++ src/genetic_algorithm/ga_pipeline.py
@@ -14,18 +14,20 @@ if __name__ == "__main__":
     logging.info(f"{os.getcwd()}")
 
     instances_path = "data/input/HRTInstances/"
-    instances_list = [x for x in range(217, 219)] #337)]
+    instances_list = [x for x in range(217, 337)]
     logging.info(f"Running for: {len(instances_list)} instances")
-    max_limit_time_sec = 10 #240
+    max_limit_time_sec = 240
     ga_results = []
 
     for instance_number in tqdm(instances_list):
         ins_x = read_input_files.read_file(
             os.path.join(instances_path, f"Instance_{instance_number}.txt")
         )
+        logging.info(f"Running GA for instance {instance_number}...")
         result = genetic_algorithm.genetic_algorithm(ins_x, max_limit_time_sec=30)
-        ga_results.append(result)
+        ga_results.append(result + (instance_number,))
         write_solution_files.write_solution_to_file(ins_x, instance_number, result)
+        logging.info(f"GA for instance {instance_number} has finished!")
 
     with open("data/solutions/all_ga.pkl", "wb") as f:
         pickle.dump(ga_results, f)
